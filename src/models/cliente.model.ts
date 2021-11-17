3e89863a5ad3770909690b1ddd09377ed85a3a1c
Nicolas <Daricksonlupe@gmail.com>
2021-11-17 11:48:11 -0500

    Primera version del proyecto pedidos

---
+++ src/models/cliente.model.ts
@@ -0,0 +1,55 @@
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Solicitud} from './solicitud.model';
+
+@model()
+export class Cliente extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Apellidos: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Telefono: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Clave: string;
+
+  @hasMany(() => Solicitud)
+  solicituds: Solicitud[];
+
+  constructor(data?: Partial<Cliente>) {
+    super(data);
+  }
+}
+
+export interface ClienteRelations {
+  // describe navigational properties here
+}
+
+export type ClienteWithRelations = Cliente & ClienteRelations;
---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+export * from './cliente.model';
+export * from './asesor.model';
+export * from './administrador.model';
+export * from './vehiculo.model';
+export * from './solicitud.model';
